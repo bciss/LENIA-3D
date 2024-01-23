91a4b915fc8215da4489302fe60aedf7f7ea417a
CISS Babacar <cissback@hotmail.fr>
2024-01-23 17:11:20 +0100

    first rules test

--- Lenia-3D/Assets/Scripts/Cube.cs
+++ Lenia-3D/Assets/Scripts/Cube.cs
@@ -7,6 +7,9 @@ public class Cube : MonoBehaviour
     public float life = 0f;
     public Material material;
     MeshRenderer mr;
+    
+    public float circle;
+    public float circlePercent;
 
     void Awake()
     {
@@ -25,6 +28,28 @@ public class Cube : MonoBehaviour
 
     public void DisplayLife()
     {
+        float circlePercent = circle / 27;
+        if (circlePercent < 0.2) {
+            life -= 0.1f;
+        }
+        else if (circlePercent >= 0.2 && circlePercent < 0.4)
+        {
+            life += 0.1f;
+        }
+        else if (circlePercent >= 0.4 && circlePercent < 0.5 )
+        {
+            life += 0.2f;
+        }
+        // else if (circlePercent >= 0.5 && circlePercent < 0.8)
+        // {
+        //     life -= 0.3f;
+        // }
+        else if (circlePercent >= 0.8)
+        {
+            life = 0;
+        }
+        life = Mathf.Clamp(life, 0, 1);
         mr.material.SetFloat("_AlphaOfLife", life);
+        circle = 0;
     }
 }
--- Lenia-3D/Assets/Scripts/PlanetController.cs
+++ Lenia-3D/Assets/Scripts/PlanetController.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class PlanetController : MonoBehaviour
 {
-    public Vector3 gridDimension = new Vector3(5, 5, 5);
+    public Vector3Int gridDimension = new Vector3Int(5, 5, 5);
+    public Cube[][][] cubes;
     public Transform grille;
     public GameObject cubePrefabs;
     public float cubeDimension = 1;
+    public float circle;
 
     // Start is called before the first frame update
     void Start()
@@ -16,12 +18,33 @@ public class PlanetController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        UpdateCircle();
+        UpdateLife();
     }
 
     public void GenerateGrid()
+    {
+        cubes = new Cube[gridDimension.x][][];
+        for (int x = 0; x < gridDimension.x; x++)
+        {
+             cubes[x] = new Cube[gridDimension.y][];
+            for (int y = 0; y < gridDimension.y; y++)
+            {
+                cubes[x][y] = new Cube[gridDimension.z];
+                for (int z = 0; z < gridDimension.y; z++)
+                {
+                    // Calculate the position for each cube
+                    Vector3 position = new Vector3(x * cubeDimension, y * cubeDimension, z * cubeDimension);
+
+                    cubes[x][y][z] = Instantiate(cubePrefabs, position, Quaternion.identity, grille).GetComponent<Cube>();
+                }
+            }
+        }
+    }
+
+    public void UpdateCircle()
     {
         for (int x = 0; x < gridDimension.x; x++)
         {
@@ -29,10 +52,51 @@ public class PlanetController : MonoBehaviour
             {
                 for (int z = 0; z < gridDimension.y; z++)
                 {
-                    // Calculate the position for each cube
-                    Vector3 position = new Vector3(x * cubeDimension, y * cubeDimension, z * cubeDimension);
+                    GetAdjacentCubes(new Vector3Int(x, y, z));
+                }
+            }
+        }
+    }
 
-                    Instantiate(cubePrefabs, position, Quaternion.identity, grille);
+    public void UpdateLife()
+    {
+        for (int x = 0; x < gridDimension.x; x++)
+        {
+            for (int y = 0; y < gridDimension.y; y++)
+            {
+                for (int z = 0; z < gridDimension.y; z++)
+                {
+                    cubes[x][y][z].DisplayLife();
+                }
+            }
+        }
+    }
+    void GetAdjacentCubes(Vector3Int targetPosition)
+    {
+        int startX = Mathf.Max(0, Mathf.FloorToInt(targetPosition.x) - 1);
+        int startY = Mathf.Max(0, Mathf.FloorToInt(targetPosition.y) - 1);
+        int startZ = Mathf.Max(0, Mathf.FloorToInt(targetPosition.z) - 1);
+
+        int endX = Mathf.Min(gridDimension.x - 1, Mathf.CeilToInt(targetPosition.x) + 1);
+        int endY = Mathf.Min(gridDimension.y - 1, Mathf.CeilToInt(targetPosition.y) + 1);
+        int endZ = Mathf.Min(gridDimension.z - 1, Mathf.CeilToInt(targetPosition.z) + 1);
+
+        for (int x = startX; x <= endX; x++)
+        {
+            for (int y = startY; y <= endY; y++)
+            {
+                for (int z = startZ; z <= endZ; z++)
+                {
+                    if (x != targetPosition.x || y != targetPosition.y || z != targetPosition.z)
+                    {
+                        // Do something with the adjacent cube at position (x, y, z)
+                        Cube adjacentCube = cubes[x][y][z];
+                        if (adjacentCube != null)
+                        {
+                            // You can perform operations on the adjacent cube here
+                            cubes[targetPosition.x][targetPosition.y][targetPosition.z].circle += adjacentCube.life;
+                        }
+                    }
                 }
             }
         }
